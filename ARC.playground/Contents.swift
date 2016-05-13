182972760c073a3fd938542d62ee484a7a206ef2
xcs <13162927913@163.com>
2016-05-13 13:39:00 +0800

    闭包引起的循环

--- ARC.playground/Contents.swift
+++ ARC.playground/Contents.swift
@@ -176,14 +176,89 @@ class City {
 var country = Country(name: "Canada", capitalName: "Ottawa")
 
 // 闭包引起的循环强引用
+// 循环强引用还会发生在当你将一个闭包赋值给类实例的某个属性，并且这个闭包体中又使用了这个类实例。
+// 循环强引用的产生，是因为闭包和类相似，都是引用类型。当把一个闭包赋值给某个属性时，也把一个引用赋值给了这个闭包。
 
+// swift提供了闭包捕获列表来解决这个问题
 
+// 产生
+class HTMLElement {
+    let name: String
+    let text: String?
+    
+    lazy var asHTML:Void -> String = {
+        if let text = self.text {
+            return "<\(self.name)> \(text) </\(self.name)>"
+        } else {
+            return "<\(self.name)>"
+        }
+    }
+    init(name:String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
 
+let heading = HTMLElement(name: "h1")
+let defaultText = "some default text"
+heading.asHTML = {
+    return "<\(heading.name)> \(heading.text ?? defaultText)<\(heading.name)>"
+}
+print(heading.asHTML())
 
+var paragraph:HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+print(paragraph!.asHTML())
 
+// 实例不会被销毁，因为存在着循环强引用
+paragraph = nil
 
+// 解决闭包引起的循环强引用
+// 在定义闭包时同时定义“捕获列表”作为闭包的一部分，通过这种方式可以解决闭包和类实例之间的循环强引用。
+// 捕获列表定义了“闭包体内捕获一个或者多个引用类型的规则”。跟解决两个实例间的循环强引用一样，声明每个捕获的引用为弱引用或无主引用，而不是强引用。
+// swift规定，只要在闭包内使用self的成员，就要用self.someProperty或者self.someMethod()
 
+// 定义捕获列表
+// 捕获列表中的每一项都由一对元素组成，一个元素是weak或unowned关键字，另一个元素是类实例的引用或初始化过的变量。
+// 如果闭包有参数列表和返回类型，把捕获列表放在它们前面
+//lazy var someClosure:(Int, String) -> String = {
+//    [unowned self, weak delegate = self.delegate!] (index: Int, stringToProcess:String) -> String in
+//}
 
+// 如果闭包没有指明参数列表或者返回类型，即它们会通过上下文推断，那么可以把捕获列表和关键字in放在闭包最开始的地方
+//lazy var someClosure:Void -> String = {
+//    [unowned self, weak delegate = self.delegate!] in
+//}
+
+// 弱引用和无主引用
+// 在闭包和捕获的实例总是互相引用时并且总是同时销毁时，将闭包内的捕获定义为无主引用。相反的，在被捕获的引用可能会变为nil时，将闭包内的捕获定义为弱引用。
+
+class HTMLElementOne {
+    let name: String
+    let text: String?
+    lazy var asHTML: Void -> String = {
+        [unowned self] in
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+    init(name:String, text:String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+var paragraphs:HTMLElementOne? = HTMLElementOne(name: "p", text: "hello, world")
+print(paragraphs!.asHTML())
+
+paragraphs = nil
 
 
 
